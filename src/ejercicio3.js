2a7c0c2e695b24b8485d36a15f0127872c3393f1
YudithM18 <pmarin@fwdcostarica.com>
2024-08-14 11:34:25 -0600

    Complete el ejercicio #3

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,4 +1,54 @@
-// Inserte el código aquí
+function clonarArreglo(arreglo) {
+    const clon = [...arreglo]
+
+    return clon
+}
+
+
+const arreglo = [1, 2, 3, 4, 5, 6]
+clonarArreglo(arreglo)
+
+
+
+
+
+function clonarObjeto(persona) {
+    const clonObj = {...persona}
+
+    return clonObj
+}
+
+const persona = {
+    nombre: "Amaya",
+    edad: 24,
+}
+clonarObjeto(persona)
+
+
+
+
+function sumar(...numbers) {
+    return numbers.reduce((acumulador, valorActual) =>{
+        return acumulador + valorActual
+    }, 0)
+    
+    
+}
+
+sumar (1,2,3,4)
+
+
+
+
+function saludar( nombre = `Desconocido`) {
+
+    return `Hola`, nombre
+    
+}
+
+
+
+
 
 
 
