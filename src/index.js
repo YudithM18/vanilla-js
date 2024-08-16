28b9526e95d76c1b87e96493b4caca06be535587
YudithM18 <pmarin@fwdcostarica.com>
2024-08-16 14:06:11 -0600

    Complete el ejercicio #4

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-// Inserte el código aquí
+import { GetUsers } from "./servicios/getUsers";
 
+resolProm()
+async function resolProm() {
+    let datosUsers = await GetUsers ()
+
+    console.log(datosUsers);
+
+}
+
+
+
+const hora = document.getElementById("hora")
+
+setInterval(() => {
+    console.log("12:00");
+    
+}, 2000);
